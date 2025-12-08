ec3b62888dc03d1f68263cdbbcc37e96bcd3e475
Kamal Baastani <kbastan@hotmail.com>
2025-12-08 16:06:39 +0330

    ++

---
+++ DataAccess/Migrations/20251208123040_add-resident.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addresident : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "Units",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "Residents",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Units");
+
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Residents");
+        }
+    }
+}
